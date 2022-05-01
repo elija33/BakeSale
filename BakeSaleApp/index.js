251fcb56884f9092256e5a9863a3c5521eb5fd8b
Elija Amponsa <amponseli@gmail.com>
2022-05-01 07:27:35 -0400

    settingup done

--- BakeSaleApp/index.js
+++ BakeSaleApp/index.js
@@ -1,9 +1,8 @@
 /**
  * @format
  */
+import App from './src/component/App';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('BakeSaleApp', () => App);
